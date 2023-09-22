0e9ece2ee0fc3ef078910182c8dcbb7c03ab2277
Anderson Madeira <andersonmadeiracs@gmail.com>
2023-09-22 10:04:17 -0700

    Use relative position to align handle to mouse position

--- scripts/Joystick.gd
+++ scripts/Joystick.gd
@@ -3,6 +3,7 @@ extends Control
 @onready var handle: TextureRect = $Handle
 @onready var arrows: TextureRect = $Arrows
 
+var handle_relative_touch_position := Vector2.ZERO
 var original_handle_global_position := Vector2.ZERO
 var handle_center_position := Vector2.ZERO
 var handle_touch_radius := 0
@@ -31,13 +32,20 @@ func _on_touch_press(touch_position: Vector2) -> void:
 	
 	if touch_distance <= handle_touch_radius:
 		is_dragging_handle = true
+		handle_relative_touch_position = _get_relative_position(handle_center_position, touch_position)
 		
 func _on_touch_release() -> void:
 	is_dragging_handle = false
 	handle.global_position = original_handle_global_position
 	
 func _on_handle_drag(drag_position: Vector2) -> void:
-	handle.global_position = drag_position  - handle.get_pivot_offset()
+	handle.global_position = drag_position - handle.get_pivot_offset() - handle_relative_touch_position
+	
+func _get_relative_position(center: Vector2, other: Vector2) -> Vector2:
+	var pos := Vector2.ZERO
+	pos.x = other.x - center.x
+	pos.y = other.y - center.y
+	return pos
 
 func _process(delta: float) -> void:
 	pass
